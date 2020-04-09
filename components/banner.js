579e6b7eb5a228f5a93c591a5b6aade9fdc338be
readeral <alan@readeral.com>
2020-04-09 21:07:45 +1000

    allowing banner to be closed

--- components/banner.js
+++ components/banner.js
@@ -1,39 +1,57 @@
 /** @jsx jsx */
-import React from 'react';
-import {jsx, ThemeProvider} from 'theme-ui';
+import React, {useState, useEffect} from 'react';
+import {jsx, ThemeProvider, Close} from 'theme-ui';
 import BlockText from './block-text-serializer';
+
 const Banner = ({header, body}) => {
+  const [hidden, setHidden] = useState(false);
+
+  const handleClose = () => {
+    sessionStorage.setItem('bannerVisible', false);
+    setHidden(true);
+  };
+
+  useEffect(() => {
+    setHidden(sessionStorage.getItem('bannerVisible'));
+  }, []);
+
   return (
-    <ThemeProvider
-      theme={{
-        styles: {
-          p: {
-            fontSize: 16,
-            fontFamily: 'body',
-            fontWeight: 'body',
-            lineHeight: 'body',
-            color: 'white',
-            margin: 0
-          },
-          a: {
-            color: 'gold',
-            ':visited': 'white'
+    !hidden && (
+      <ThemeProvider
+        theme={{
+          styles: {
+            p: {
+              fontSize: 16,
+              fontFamily: 'body',
+              fontWeight: 'body',
+              lineHeight: 'body',
+              color: 'white',
+              margin: 0
+            },
+            a: {
+              color: 'gold',
+              ':visited': 'white'
+            }
           }
-        }
-      }}
-    >
-      <div
-        sx={{
-          backgroundColor: 'alt',
-          display: 'block',
-          textAlign: 'center',
-          padding: '15px',
-          color: 'white'
         }}
       >
-        <BlockText blocks={body} />
-      </div>
-    </ThemeProvider>
+        <div
+          sx={{
+            backgroundColor: 'alt',
+            display: 'block',
+            textAlign: 'center',
+            padding: '15px',
+            color: 'white'
+          }}
+        >
+          <BlockText blocks={body} />
+          <Close
+            sx={{position: 'absolute', top: '20px', right: '30px'}}
+            onClick={handleClose}
+          />
+        </div>
+      </ThemeProvider>
+    )
   );
 };
 

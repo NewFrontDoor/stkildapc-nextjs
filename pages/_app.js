3de03ddede7f10f1c0725e032f2d3cf3be0a0b8d
Bratalix <bratalix@gmail.com>
2020-06-03 11:49:48 +1000

    Adding google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,39 +1,34 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, {useEffect} from 'react';
 import App from 'next/app';
+import Router from 'next/router'
+import * as gtag from '../lib/google-analytics'
 import {ThemeProvider, Styled} from 'theme-ui';
 import theme from '../theme';
 require('typeface-lato'); //eslint-disable-line
 require('typeface-open-sans'); //eslint-disable-line
 
-class MyApp extends App {
-  // Only uncomment this method if you have blocking data requirements for
-  // every single page in your application. This disables the ability to
-  // perform automatic static optimization, causing every page in your app to
-  // be server-side rendered.
-  //
-  // static async getInitialProps(appContext) {
-  //   // calls page's `getInitialProps` and fills `appProps.pageProps`
-  //   const appProps = await App.getInitialProps(appContext);
-  //
-  //   return { ...appProps }
-  // }
 
-  render() {
-    const {Component, pageProps} = this.props;
-    return (
-      <ThemeProvider theme={theme}>
+const MyApp = ({ Component, pageProps }) => {
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    Router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      Router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [])
+
+  return(
+    <ThemeProvider theme={theme}>
         <Styled.root>
           <Component {...pageProps} />
         </Styled.root>
       </ThemeProvider>
-    );
-  }
+  )
 }
 
-MyApp.propTypes = {
-  Component: PropTypes.elementType.isRequired,
-  pageProps: PropTypes.any.isRequired
-};
+export default MyApp
 
-export default MyApp;
+

83dd4d3ec389b46faace846f3d6a3d684973e21e
readeral <alan@readeral.com>
2020-07-25 22:38:03 +1000

    reverting await

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -13,17 +13,17 @@ async function testSlug(slug) {
 export default async function preview(req, res) {
   // Check the secret and next parameters
   // This secret should only be known to this API route and the CMS
-  const val = await req;
+  console.log(inspect(req, {getters: true, depth: 2}))
 
   if (
-    val.query.secret !== process.env.SANITY_PREVIEW_SECRET ||
-    !val.query.slug
+    req.query.secret !== process.env.SANITY_PREVIEW_SECRET ||
+    !req.query.slug
   ) {
     return res.status(401).json({message: 'Invalid token'});
   }
 
   // Fetch the headless CMS to check if the provided `slug` exists
-  const post = await testSlug(val.query.slug);
+  const post = await testSlug(req.query.slug);
 
   // If the slug doesn't exist prevent preview mode from being enabled
   if (!post) {

16ca6dae48dacedc834e196779066aded395599f
Bratalix <bratalix@gmail.com>
2024-06-13 16:47:34 +1000

    take 2 env

--- next.config.js
+++ next.config.js
@@ -1,27 +1,25 @@
 // Next.config.js
-const withPlugins = require('next-compose-plugins');
-const withImages = require('next-images');
-const withFonts = require('next-fonts');
+const withPlugins = require("next-compose-plugins");
+const withImages = require("next-images");
+const withFonts = require("next-fonts");
 
 const nextConfig = {
-  env: {
-    GOOGLE_MAPS_API: process.env.GOOGLE_MAPS_API,
-    RESTRICTED_PAGES_PASSWORD: process.env.RESTRICTED_PAGES_PASSWORD,
-    NEXT_PUBLIC_VERCEL_URL: process.env.NEXT_PUBLIC_VERCEL_URL,
-    STKILDA_SENDGRID: process.env.STKILDA_SENDGRID,
-    STKILDA_SANITY: process.env.STKILDA_SANITY,
-    PREVIEW_TOKEN: process.env.PREVIEW_TOKEN,
-    SANITY_PREVIEW_SECRET: process.env.SANITY_PREVIEW_SECRET,
-    NEXT_PUBLIC_VERCEL_URL: process.env.NEXT_PUBLIC_VERCEL_URL
-  },
-  webpack: (config, options) => {
-    // Modify the `config` here
+	env: {
+		GOOGLE_MAPS_API: process.env.GOOGLE_MAPS_API,
+		RESTRICTED_PAGES_PASSWORD: process.env.RESTRICTED_PAGES_PASSWORD,
+		NEXT_PUBLIC_VERCEL_URL: process.env.NEXT_PUBLIC_VERCEL_URL,
+		STKILDA_SENDGRID: process.env.STKILDA_SENDGRID,
+		STKILDA_SANITY: process.env.STKILDA_SANITY,
+		PREVIEW_TOKEN: process.env.PREVIEW_TOKEN,
+		SANITY_PREVIEW_SECRET: process.env.SANITY_PREVIEW_SECRET,
+		NEXT_PUBLIC_VERCEL_URL: process.env.NEXT_PUBLIC_VERCEL_URL,
+		VERCEL_ENV: process.env.VERCEL_ENV
+	},
+	webpack: (config, options) => {
+		// Modify the `config` here
 
-    return config;
-  }
+		return config;
+	}
 };
 
-module.exports = withPlugins(
-  [withFonts, [withImages, { ignoreTypes: ['svg'] }]],
-  nextConfig
-);
+module.exports = withPlugins([withFonts, [withImages, { ignoreTypes: ["svg"] }]], nextConfig);
--- pages/robots.txt.js
+++ pages/robots.txt.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const crawlableRobotsTxt = `User-agent: *\nAllow: /`;
+const crawlableRobotsTxt = `User-agent: *\nAllow: /\nDisallow: /restricted/`;
 
 const uncrawlableRobotsTxt = `User-agent: *\nDisallow: /`;
 
@@ -8,7 +8,7 @@ class Robots extends React.Component {
 	static async getInitialProps({ res }) {
 		res.setHeader("Content-Type", "text/plain");
 		// Return a non-crawlable robots.txt in non-production environments
-		res.write(process.env.NEXT_PUBLIC_VERCEL_ENV === "production" ? crawlableRobotsTxt : uncrawlableRobotsTxt);
+		res.write(process.env.VERCEL_ENV === "production" ? crawlableRobotsTxt : uncrawlableRobotsTxt);
 		res.end();
 	}
 }

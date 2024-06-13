1020eb0bc94004f6b27e64b6ddc908a7c4812dfc
Bratalix <bratalix@gmail.com>
2024-06-13 16:39:26 +1000

    env var

--- pages/robots.txt.js
+++ pages/robots.txt.js
@@ -1,19 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const crawlableRobotsTxt = `User-agent: *\nAllow: /`;
 
 const uncrawlableRobotsTxt = `User-agent: *\nDisallow: /`;
 
 class Robots extends React.Component {
-  static async getInitialProps({ res }) {
-    res.setHeader('Content-Type', 'text/plain');
-    // Return a non-crawlable robots.txt in non-production environments
-    res.write(process.env.VERCEL_ENV === "production"
-      ? crawlableRobotsTxt
-      : uncrawlableRobotsTxt
-    );
-    res.end();
-  }
+	static async getInitialProps({ res }) {
+		res.setHeader("Content-Type", "text/plain");
+		// Return a non-crawlable robots.txt in non-production environments
+		res.write(process.env.NEXT_PUBLIC_VERCEL_ENV === "production" ? crawlableRobotsTxt : uncrawlableRobotsTxt);
+		res.end();
+	}
 }
 
 export default Robots;
